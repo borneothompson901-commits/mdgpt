d15f93fb328cdfe4c49f9acff8862f29763824e7
borneothompson901-commits <borneo.thompson901@gmail.com>
2026-03-07 14:37:10 +0700

    Update auth_guard.php

--- cms/auth_guard.php
+++ cms/auth_guard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 if (!isset($_SESSION["admin"]) || $_SESSION["admin"] !== true) {
-  header("Location: /mdgpt/cms/login.php");
+  header("Location: /cms/login.php");
   exit;
 }

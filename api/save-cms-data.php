d0545aaeedcfa91607609b39fb71d6e15a76a86d
borneothompson901-commits <borneo.thompson901@gmail.com>
2026-08-12 07:21:17 +0700

    Update save-cms-data.php

--- api/save-cms-data.php
+++ api/save-cms-data.php
@@ -1,7 +1,6 @@
 <?php
-session_start();
+require __DIR__ . "/../config/session_init.php";
 header("Content-Type: application/json");
-require __DIR__ . "/../config/database.php";
 
 // Check auth
 if (!isset($_SESSION["admin"]) || $_SESSION["admin"] !== true) {
